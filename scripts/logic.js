b48d723cab9a849a90b29478140d9c0dbbd8e37a
Michael Schreier <mjschreier1@gmail.com>
2018-01-12 16:01:11 -0700

    completed project

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,14 +1,42 @@
+var randomX;
+var randomY;
+reassignCoordinates();
+
 function fireMissle(displayBoard, board, coordinates) {
+  if(board[coordinates.y][coordinates.x] !== e) {
+    displayBoard[coordinates.y][coordinates.x] = h; 
+  } else {
+    displayBoard[coordinates.y][coordinates.x] = m;
+  }
   return displayBoard
 }
 
 function generateComputerMove(board) {
+  while (Math.abs(board[randomY][randomX]) === 1) {
+    reassignCoordinates();
+  }
   return {
-    x: 0,
-    y: 0
+    x: randomX,
+    y: randomY
   }
 }
 
 function isWon(board) {
+  var hits = [];
+  board.forEach(array => {
+    array.forEach(square => {
+      if (square === -1) {
+        hits.push(square);
+      }
+    });
+  });
+  if(hits.length === 17) {
+    return true
+  }
   return false
+}
+
+function reassignCoordinates() {
+  randomX = Math.floor(Math.random() * 10);
+  randomY = Math.floor(Math.random() * 10);
 }
